e67ac35b29846bcd59e900299a2df37b6d930714
eke9813@umoncton.ca <eke9813@umoncton.ca>
2026-03-28 14:17:24 -0300

    feat(05-08): add /dashboard/build-test page
    - Mounts BuildTestWizard inside max-w-3xl container
    - Route destination for Build New Test CTA from /dashboard/tests

---
+++ src/app/(dashboard)/dashboard/build-test/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { BuildTestWizard } from '@/components/dashboard/BuildTestWizard'
+
+export default function BuildTestPage() {
+  return (
+    <div className="max-w-3xl mx-auto">
+      <BuildTestWizard />
+    </div>
+  )
+}
